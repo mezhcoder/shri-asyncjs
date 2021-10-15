a0570039632820adf5174af08b2700dee24dd7ca
zhcoder <zhcoder@yandex.ru>
2021-10-15 21:55:28 +0300

    update

--- playground.js
+++ playground.js
@@ -45,49 +45,6 @@
     Object.freeze(global.Homework);
 })(typeof window === 'undefined' ? global : window);
 
-const { AsyncArray, add, subtract, multiply, divide, less, equal, lessOrEqual } = Homework;
-const a = new AsyncArray([1, 2, 3]);
-
-// a.push(4, () => {
-//     console.log('добавление элемента выполнено');
-//     a.print();
-//
-//     a.set(2, 999, () => {
-//         console.log('присваивание элемента по индексу выполнено');
-//         a.print();
-//
-//         a.get(0, (result) => {
-//             console.log('получение элемента по индексу выполнено, результат', result);
-//             a.print();
-//
-//             a.pop((result) => {
-//                 console.log('получение последнего элемента выполнено, результат', result);
-//                 a.print();
-//
-//                 a.length((result) => {
-//                     console.log('получение длины массива выполнено, результат', result);
-//                     a.print();
-//                 });
-//             });
-//         });
-//     });
-// });
-
-// add(5, 2, (result) => console.log('результат сложения', result));
-// subtract(11, 7, (result) => console.log('результат вычитания', result));
-// multiply(6, 7, (result) => console.log('результат умножения', result));
-// divide(13, 7, (result) => console.log('результат деления', result));
-// less(5, 3, (result) => console.log('результат операции МЕНЬШЕ', result));
-// equal(1, 1, (result) => console.log('результат операции РАВНО', result));
-// lessOrEqual(12, 19, (result) => console.log('результат операции МЕНЬШЕ ИЛИ РАВНО', result));
-
-const asyncArray = new Homework.AsyncArray([1, 2, 3, 4]);
-const reducerSum = (acc, curr, i, src, cb) => Homework.add(acc, curr, cb);
-
-reduce(asyncArray, reducerSum, 0, (res) => {
-    console.log(res) // 10
-})
-
 function reduce(asyncArray, fn, initialValue, cb) {
     async function run() {
         const length = () => new Promise( function(resolve, reject) {
--- solution/index.js
+++ solution/index.js
@@ -1,25 +1,15 @@
+// solution/index.js
 module.exports = function (Homework) {
-    const {
-        AsyncArray,
-        add,
-        subtract,
-        multiply,
-        divide,
-        less,
-        equal,
-        lessOrEqual,
-    } = Homework;
-
+    const { AsyncArray, add, subtract, multiply, divide, less, equal, lessOrEqual } = Homework;
     return (array, fn, initialValue, cb) => {
         async function run() {
             const length = () => new Promise( function(resolve, reject) {
-                asyncArray.length(resolve);
+                array.length(resolve);
             });
 
             const pop = () => new Promise( function(resolve, reject) {
-                asyncArray.pop(resolve);
+                array.pop(resolve);
             });
-
 
             while (await length()) {
                 const element = await pop();
